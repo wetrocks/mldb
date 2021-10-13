be1bc3aa438e6ebadba24ea3fcc61a58f166aa43
wetrocks <25932261+wetrocks@users.noreply.github.com>
2021-10-13 19:01:46 -0400

    Initial add of integration test project

---
+++ MLDB.Api.IntegrationTests/APITestWebApplicationFactory.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace MLDB.Api.IntegrationTests
+{
+    public class APITestWebApplicationFactory : WebApplicationFactory<MLDB.Api.Startup>
+    {
+        
+    }
+}
