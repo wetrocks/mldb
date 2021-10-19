47d00324fea82f63a3cc2242df8cb0a7be89dbf2
wetrocks <25932261+wetrocks@users.noreply.github.com>
2021-10-18 22:23:15 -0400

    Added partial SurveyDTO mapping to Site -> SiteDTO mapping

--- MLDB.Api.Tests/MappingTests/SiteMappingTests.cs
+++ MLDB.Api.Tests/MappingTests/SiteMappingTests.cs
@@ -6,6 +6,7 @@ using MLDB.Api.Models;
 using FluentAssertions;
 using AutoFixture;
 using MLDB.Api.Mapping;
+using System.Collections.Generic;
 
 namespace MLDB.Api.Tests.MappingTests {
     public class SiteMappingTests {
@@ -25,6 +26,7 @@ namespace MLDB.Api.Tests.MappingTests {
         public void siteMapping_ToDTO_ShouldMapExpectedFields() {
             var testSite = fixture.Build<Site>()
                                 .With( x => x.CreateUser )
+                                .Without( x => x.Surveys )
                                 .Create();
 
             // Perform mapping
@@ -36,6 +38,30 @@ namespace MLDB.Api.Tests.MappingTests {
         }
 
         [Test]
+        public void siteMapping_ToDTO_ShouldIncludeSurveyInfo() {
+
+            var testSurvey = fixture.Build<Survey>()
+                                    .With( x => x.StartTimeStamp, new DateTime(1969,4,20,16,20,09))
+                                    .Create();
+
+            var testSite = fixture.Build<Site>()
+                                .With( x => x.CreateUser )
+                                .With( x => x.Surveys, new List<Survey>() { testSurvey })
+                                .Create();
+
+            // Perform mapping
+            var siteDTO  = mapper.Map<SiteDTO>(testSite);
+
+            siteDTO.Surveys.Should().HaveCount(1);
+            siteDTO.Surveys[0].Id.Should().Be(testSurvey.Id);
+            siteDTO.Surveys[0].SurveyDate.Should().Be("1969-04-20");
+            siteDTO.Surveys[0].Coordinator.Should().Be(testSurvey.Coordinator);
+            siteDTO.Surveys[0].VolunteerCount.Should().Be(testSurvey.VolunteerCount);
+            siteDTO.Surveys[0].TotalKg.Should().Be(testSurvey.TotalKg);
+            siteDTO.Surveys[0].LitterItems.Should().BeNullOrEmpty();
+        }
+
+        [Test]
         public void siteMapping_FromDTO_ShouldMapExpectedFields()  {
             var testDTO = fixture.Build<SiteDTO>().Create();
 
@@ -43,6 +69,7 @@ namespace MLDB.Api.Tests.MappingTests {
 
             testSite.Id.Should().Be(testDTO.Id);
             testSite.Name.Should().Be(testDTO.Name);
+            testSite.Surveys.Should().BeNullOrEmpty();
         }
     }
 }
--- MLDB.Api/DTO/SiteDTO.cs
+++ MLDB.Api/DTO/SiteDTO.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Collections.Generic;
 
 namespace MLDB.Api.DTO {
     public record SiteDTO
     {
         public Guid Id { get; init; }
         public string Name { get; init; }
-        public string CreatedBy{ get; init; }
+        public string CreatedBy { get; init; }
+        public List<SurveyDTO> Surveys { get; init; }
     };
 }
