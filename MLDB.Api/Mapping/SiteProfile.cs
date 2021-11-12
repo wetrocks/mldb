ac0b6cef4a76054dd20229e3f9e79f511d933407
wetrocks <25932261+wetrocks@users.noreply.github.com>
2021-11-12 12:36:05 -0400

    Further refactor, removed old Site/Survey

--- MLDB.Api/Mapping/SiteProfile.cs
+++ MLDB.Api/Mapping/SiteProfile.cs
@@ -1,6 +1,6 @@
 using AutoMapper;
 using MLDB.Api.DTO;
-using MLDB.Api.Models;
+using MLDB.Domain;
 
 
 namespace MLDB.Api.Mapping {
@@ -8,17 +8,8 @@ namespace MLDB.Api.Mapping {
     public class SiteProfile : Profile
     {
         public SiteProfile() {
-            CreateMap<Survey, SurveyDTO>()
-                .ForMember( x => x.SurveyDate, s => s.MapFrom( s => s.StartTimeStamp.ToString("yyyy-MM-dd")) )
-                .ForMember( x => x.LitterItems, opt => opt.Ignore() );
-
-
             CreateMap<Site, SiteDTO>()
-                .ForMember( x => x.CreatedBy, s => s.MapFrom( x => x.CreateUser != null ? x.CreateUser.Name : null ))                  
-                .ReverseMap()
-                .ForMember( x => x.Surveys, opt => opt.Ignore() );
-
-            CreateMap<MLDB.Domain.Site, SiteDTO>()
+                    .ForMember( x => x.CreatedBy, s => s.MapFrom( x => x.CreateUserId ))
                     .ReverseMap()
                     .ForCtorParam("name", opt => opt.MapFrom(src => src.Name))
                     .ForCtorParam("createUserId", opt => opt.MapFrom(src => src.CreatedBy));
--- MLDB.Api/Mapping/SurveyProfile.cs
+++ MLDB.Api/Mapping/SurveyProfile.cs
@@ -1,31 +1,43 @@
 using AutoMapper;
 using MLDB.Api.DTO;
-using MLDB.Api.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System;
 using System.Globalization;
+using MLDB.Domain;
 
-namespace MLDB.Api.Mapping {
+namespace MLDB.Api.Mapping
+{
 
     public class SurveyProfile : Profile
     {
-        public SurveyProfile() {
-            CreateMap<Survey, SurveyDTO>()
-            .ForMember( x => x.LitterItems, s => s.MapFrom( x => x.LitterItems.ToDictionary( li => li.LitterTypeId,
-                                                                                             li => li.Count )))
-            .ForMember( x => x.SurveyDate, s => s.MapFrom( x => x.StartTimeStamp.ToString("yyyy-MM-dd")))                                                                                
-            .ForMember( x => x.StartTime, s => s.MapFrom( x => x.StartTimeStamp.ToString("T", CultureInfo.InvariantCulture)))
-            .ForMember( x => x.EndTime, s => s.MapFrom( x => x.EndTimeStamp.ToString("T", CultureInfo.InvariantCulture)))
-            .ReverseMap()
-            .ForMember( x => x.StartTimeStamp, s => s.MapFrom( x => $"{x.SurveyDate}T{x.StartTime ?? "00:00:00"}" ))
-            .ForMember( x => x.EndTimeStamp, s => s.MapFrom( x => $"{x.SurveyDate}T{x.EndTime ?? "00:00:00"}" ))
-            .ForMember( x => x.LitterItems, s => s.MapFrom( x => x.LitterItems.Select(
-                item => new LitterItem { SurveyId = x.Id, LitterTypeId = Int16.Parse(item.Key), Count = item.Value }
-            )));
-
+        public SurveyProfile()
+        {
             CreateMap<DateTime, string>().ConvertUsing(dt => dt.ToString("s"));
             CreateMap<string, DateTime>().ConvertUsing(s => DateTime.ParseExact(s, "s", CultureInfo.InvariantCulture));
+
+            CreateMap<Survey, SurveyDTO>()
+                .ForMember(x => x.Coordinator, s => s.MapFrom(x => x.CoordinatorName))
+                .ForMember(x => x.SurveyDate, s => s.MapFrom(x => x.StartTimeStamp.ToString("yyyy-MM-dd")))
+                .ForMember(x => x.StartTime, s => s.MapFrom(x => x.StartTimeStamp.ToString("T", CultureInfo.InvariantCulture)))
+                .ForMember(x => x.EndTime, s => s.MapFrom(x => x.EndTimeStamp.ToString("T", CultureInfo.InvariantCulture)))
+                .ForMember(x => x.LitterItems,
+                    s => s.MapFrom(x => x.LitterItems.ToDictionary(li => li.LitterTypeId, li => li.Count)));
+
+            CreateMap<SurveyDTO, Survey>()
+                .ConstructUsing( x => new Survey(x.Id, new List<int> { 42, 43 }, "testuser"))
+                .ForCtorParam("siteId", s => s.MapFrom(x => x.Id))
+                .ForCtorParam("createUserId", s => s.MapFrom(x => x.Coordinator))
+                .ForMember(x => x.CoordinatorName, s => s.MapFrom(x => x.Coordinator))
+                .ForMember( x => x.StartTimeStamp, s => s.MapFrom( x => $"{x.SurveyDate}T{x.StartTime ?? "00:00:00"}" ))
+                .ForMember( x => x.EndTimeStamp, s => s.MapFrom( x => $"{x.SurveyDate}T{x.EndTime ?? "00:00:00"}" ))
+                .ForMember( x => x.CreateUserId, s => s.Ignore())
+                .ForMember( x => x.CreateTimestamp, s => s.Ignore())
+                .ForMember( x => x.LitterItems, s => s.Ignore())
+                .AfterMap((src, dest) => { 
+                    src.LitterItems.AsEnumerable().ToList().ForEach( x => 
+                        dest.updateLitterCount(int.Parse(x.Key), x.Value));
+                });
         }
     }
 }
