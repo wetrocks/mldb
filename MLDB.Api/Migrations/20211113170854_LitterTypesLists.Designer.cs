8741b5d2a82f35c53be95b28865073658c35d17d
wetrocks <25932261+wetrocks@users.noreply.github.com>
2021-11-13 13:16:00 -0400

    re-added data seeding for litter types

--- MLDB.Api/Migrations/20211113170854_LitterTypesLists.Designer.cs
+++ MLDB.Api/Migrations/20211113170854_LitterTypesLists.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using MLDB.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MLDB.Api.Migrations
 {
     [DbContext(typeof(SiteSurveyContext))]
-    partial class SiteSurveyContextModelSnapshot : ModelSnapshot
+    [Migration("20211113170854_LitterTypesLists")]
+    partial class LitterTypesLists
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- MLDB.Infrastructure.Repositories/SiteSurveyContext.cs
+++ MLDB.Infrastructure.Repositories/SiteSurveyContext.cs
@@ -1,12 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using MLDB.Domain;
-using Newtonsoft.Json;
-using System.Collections.Generic;
-using System.IO;
-using Microsoft.Extensions.FileProviders;
-using System.Reflection;
-using System.Linq;
-using System;
+using MLDB.Infrastructure.Repositories.Configuration;
 
 namespace MLDB.Infrastructure.Repositories
 {
@@ -17,8 +11,6 @@ namespace MLDB.Infrastructure.Repositories
         {
         }
 
-
-
         public DbSet<Survey> Surveys { get; set; }
 
         // public DbSet<User> Users { get;  set; }
@@ -27,11 +19,10 @@ namespace MLDB.Infrastructure.Repositories
 
         public DbSet<Site> Sites { get; set; }
 
-        // public DbSet<LitterType> LitterTypes { get;  set; }
+        public DbSet<LitterType> LitterTypes { get;  set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
             modelBuilder.Entity<SurveyTemplate>()
                         .HasMany( st => st.LitterTypes )
                         .WithMany("Surveys");
@@ -48,47 +39,12 @@ namespace MLDB.Infrastructure.Repositories
                         .WithMany()
                         .HasForeignKey( x => x.SiteId );
 
-        //     this.PopulateSurveyTemplates(modelBuilder);
-        // }
-        // private void PopulateSurveyTemplates(ModelBuilder builder)  
-        // {  
-        //     var embeddedProvider = new EmbeddedFileProvider(Assembly.GetExecutingAssembly());
-        //     using (var fileStream = embeddedProvider.GetFileInfo("seedData/seedLitterTypes.json").CreateReadStream())
-        //     {
-        //         var templates = ReadSurveyTemplates(fileStream);
-     
-        //         builder.Entity<SurveyTemplate>()
-        //             .HasData(templates.Select( x => new SurveyTemplate { Id = x.Id, Description = x.Description}));
-
-        //         var allTypes = templates.Aggregate(new List<LitterType>(),
-        //                 (typeList,template) => typeList.Union(template.LitterTypes).ToList());
-
-        //         builder.Entity<LitterType>().HasData(allTypes);
-
-        //         // this seem weird but its apparently how its done
-        //         var templateTypes = templates.Aggregate(new List<(string,int)>(), 
-        //                 (relList, template) => {
-        //                     var relationships = template.LitterTypes.Select( x => (template.Id, x.Id ));
-
-        //                     return relList.Concat(relationships).ToList();
-        //                 });
-        //         builder.Entity<SurveyTemplate>()
-        //             .HasMany( x => x.LitterTypes)
-        //             .WithMany( y => y.SurveyTemplates)
-        //             .UsingEntity( j => j.HasData(templateTypes.Select( x => 
-        //                 new { SurveyTemplatesId = x.Item1, LitterTypesId = x.Item2} )));
-        //     }
-        } 
-
-        // public List<SurveyTemplate> ReadSurveyTemplates(Stream jsonStream) {
-        //     var templates = new List<SurveyTemplate>();
-        //     using (StreamReader r = new StreamReader(jsonStream))
-        //     {
-        //         var json = r.ReadToEnd();
-        //         templates = JsonConvert.DeserializeObject<List<SurveyTemplate>>(json);
-        //     }
-
-        //     return templates;
-        // }
+            // read lists of litter types
+            var litterTypesLists = DataLoad.ReferenceDataLoader.readLitterTypes();
+            
+            var litterTypeConfigs =  new LitterTypeEntityConfigurations(litterTypesLists);
+            litterTypeConfigs.Configure(modelBuilder.Entity<LitterType>());
+            litterTypeConfigs.Configure(modelBuilder.Entity<DataLoad.LitterTypesList>());
+        }
     }
 }
