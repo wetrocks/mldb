77f153a7362c6cca0b89c00bd1616b0b684a82de
Aimee Young <aimeesessler@gmail.com>
2021-12-17 11:58:24 -0400

    added more required fields

--- MLDB.Api/DTO/SiteDTO.cs
+++ MLDB.Api/DTO/SiteDTO.cs
@@ -9,6 +9,40 @@ namespace MLDB.Api.DTO {
         public string beachName { get; init; }
         public string beachCode { get; init; }
         public string countryCode { get; init; }
+        public string lowTideWidth { get; init; }
+        public string highTideWidth { get; init; }
+        public string beachLength { get; init; }
+        public string beachBack { get; init; }
+        public string startGPS { get; init; }
+        public string endGPS { get; init; }
+        public string quadrant1 { get; init; }
+        public string quadrant2 { get; init; }
+        public string quadrant3 { get; init; }
+        public string coordinationSystem { get; init; }
+        public string dateGPSPositionMeasured{ get; set; }
+
+        public string prevailingCurrent{ get; set; }
+
+        public string prevailingWinds{ get; set; }
+
+        public string beachDirection{ get; set; }
+
+        public string beachMaterial{ get; set; }
+
+        public string beachTopography{ get; set; }
+
+        public string beachCurvature{ get; set; }
+
+        public string horizontalBeachProfile{ get; set; }
+
+        public string seaObjects{ get; set; }
+
+        public string majorUsage{ get; set; }
+
+        public string beachVisitsPerYear{ get; set; }
+
+        public string beachAccess{ get; set; }
+    
         public string CreatedBy { get; init; }
         public List<SurveyDTO> Surveys { get; init; }
     };
--- MLDB.Domain/Site.cs
+++ MLDB.Domain/Site.cs
@@ -15,6 +15,50 @@ namespace MLDB.Domain
 
         public string countryCode{ get; set; }
 
+        public string lowTideWidth{ get; set; }
+
+        public string highTideWidth{ get; set; }
+
+        public string beachLength{ get; set; }
+
+        public string beachBack{ get; set; }
+
+        public string startGPS{ get; set; }
+
+        public string endGPS{ get; set; }
+
+        public string quadrant1{ get; set; }
+
+        public string quadrant2{ get; set; }
+
+        public string quadrant3{ get; set; }
+
+        public string coordinationSystem{ get; set; }
+
+        public string dateGPSPositionMeasured{ get; set; }
+
+        public string prevailingCurrent{ get; set; }
+
+        public string prevailingWinds{ get; set; }
+
+        public string beachDirection{ get; set; }
+
+        public string beachMaterial{ get; set; }
+
+        public string beachTopography{ get; set; }
+
+        public string beachCurvature{ get; set; }
+
+        public string horizontalBeachProfile{ get; set; }
+
+        public string seaObjects{ get; set; }
+
+        public string majorUsage{ get; set; }
+
+        public string beachVisitsPerYear{ get; set; }
+
+        public string beachAccess{ get; set; }
+    
         public string CreateUserId{ get; init; }
 
         public  DateTime CreateTimestamp { get; init; }
