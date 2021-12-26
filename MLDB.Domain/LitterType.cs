697da14892c0f2cb380e1ce45e92d2626a79b6f6
wetrocks <25932261+wetrocks@users.noreply.github.com>
2021-12-26 12:20:25 -0400

    Added LitterTypeRepository

--- MLDB.Domain/LitterType.cs
+++ MLDB.Domain/LitterType.cs
@@ -6,21 +6,14 @@ namespace MLDB.Domain
 {
     public class LitterType
     {
-        public int Id { get; private set; }
+        public UInt32 Id { get; init; }
 
-        public int OsparId { get; private set; }
+        public UInt32? OsparId { get; init; }
 
-        public string DadId { get; private set; }
+        public String OsparCategory { get; init; }
 
-        public string Description { get; private set; }
+        public string Description { get; init; }
 
         public LitterSourceCategory SourceCategory { get; init; }
-
-        public LitterType( int id, int osparId, string dadId, String description) {
-            this.Id = id;
-            this.OsparId = osparId;
-            this.DadId = dadId;
-            this.Description = description;
-        }
     }
 }
---
+++ MLDB.Infrastructure.Repositories/LitterTypeRepository.cs
@@ -0,0 +1,55 @@
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using  MLDB.Domain;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace MLDB.Infrastructure.Repositories
+{
+    public class LitterTypeRepository : ILitterTypeRepository
+    {
+
+        private readonly SiteSurveyContext _dbCtx;
+
+        public LitterTypeRepository(SiteSurveyContext dbContext) {
+            _dbCtx = dbContext;
+        }
+
+        public async Task<List<LitterType>> getAll()
+        {
+            var internalTypes = await _dbCtx.InternalLitterTypes
+                                            .Include( x => x.SourceCategory)
+                                            .Include( x => x.OsparMapping)
+                                            .ThenInclude( m => m.MappedType)
+                                            .AsNoTracking()
+                                            .ToListAsync();
+
+            
+            return internalTypes.Select(InternalToLitterType).ToList();
+        }
+
+        public async Task<LitterType> findAsync(UInt32 id)
+        {
+            var internalType = await _dbCtx.InternalLitterTypes
+                                            .Include( x => x.SourceCategory)
+                                            .Include( x => x.OsparMapping)
+                                            .ThenInclude( m => m.MappedType)
+                                            .AsNoTracking()
+                                            .SingleOrDefaultAsync( x => x.Code == id);
+            if( internalType == null )
+                return null;
+
+            return InternalToLitterType(internalType);
+        }
+
+        private LitterType InternalToLitterType(Model.InternalLitterType internalType)
+        {
+            return new LitterType { Id = internalType.Code, 
+                                    Description = internalType.Description,
+                                    SourceCategory = internalType.SourceCategory,
+                                    OsparId = internalType.OsparMapping?.MappedType?.Code,
+                                    OsparCategory = internalType.OsparMapping?.MappedType?.Category};
+        }
+    }
+}
