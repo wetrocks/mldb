4103f692b5299c8855d2f7b75b7b8e17e5bc4e4a
Aimee Young <aimeesessler@gmail.com>
2022-01-09 19:46:13 -0400

    update to saving a new site.

--- MLDB.Api/DTO/SiteDTO.cs
+++ MLDB.Api/DTO/SiteDTO.cs
@@ -18,94 +18,94 @@ namespace MLDB.Api.DTO {
         public string quadrant1 { get; init; }
         public string quadrant2 { get; init; }
         public string quadrant3 { get; init; }
-        public string coordinationSystem { get; init; }
-        public string dateGPSPositionMeasured{ get; set; }
-        public string prevailingCurrent{ get; set; }
-        public string prevailingWinds{ get; set; }
-        public string beachDirection{ get; set; }
-        public string beachMaterial{ get; set; }
-        public string beachTopography{ get; set; }
-        public string beachCurvature{ get; set; }
-        public string horizontalBeachProfile{ get; set; }
-        public string seaObjects{ get; set; }
-        public string majorUsage{ get; set; }
-        public string beachVisitsPerYear{ get; set; }
-        public string beachAccess{ get; set; }
+    //     public string coordinationSystem { get; init; }
+    //     public string dateGPSPositionMeasured{ get; set; }
+     //      public string prevailingCurrent{ get; set; }
+         public string prevailingWinds{ get; set; }
+    //     public string beachDirection{ get; set; }
+    //     public string beachMaterial{ get; set; }
+    //     public string beachTopography{ get; set; }
+    //     public string beachCurvature{ get; set; }
+    //     public string horizontalBeachProfile{ get; set; }
+    //     public string seaObjects{ get; set; }
+    //     public string majorUsage{ get; set; }
+    //     public string beachVisitsPerYear{ get; set; }
+    //     // public string beachAccess{ get; set; }
 
-        public string nearestTown{ get; set; }
-        public string townPosition{ get; set; }
+    //     public string nearestTown{ get; set; }
+    //     // public string townPosition{ get; set; }
 
-        public string townPopulation{ get; set; }
+    //     public string townPopulation{ get; set; }
 
-        public string developmentBehindBeach{ get; set; }
+    //     public string developmentBehindBeach{ get; set; }
 
-        public string developmentBehindBeachDesc{ get; set; }
+    //     public string developmentBehindBeachDesc{ get; set; }
 
-        public string foodOnBeach{ get; set; }
+    //     public string foodOnBeach{ get; set; }
 
-        public string foodDistance{ get; set; }
+    //     public string foodDistance{ get; set; }
 
-        public string foodYearRound{ get; set; }
+    //     public string foodYearRound{ get; set; }
 
-        public string foodMonths{ get; set; }
+    //     //public string foodMonths{ get; set; }
 
-        public string foodPosition{ get; set; }
+    //     // public string foodPosition{ get; set; }
 
-        public string nearestShippingLane{ get; set; }
+    //     public string nearestShippingLane{ get; set; }
 
-        public string shippingLaneDensity{ get; set; }
+    //     public string shippingLaneDensity{ get; set; }
 
-        public string shippingLaneUse{ get; set; }
+    //     public string shippingLaneUse{ get; set; }
 
-        public string shippingLanePosition{ get; set; }
+    //     // public string shippingLanePosition{ get; set; }
 
-        public string nearestHarbour{ get; set; }
+    //     public string nearestHarbour{ get; set; }
 
-        public string harbourName{ get; set; }
+    //     public string harbourName{ get; set; }
 
-        public string harbourType{ get; set; }
+    //     public string harbourType{ get; set; }
 
-        public string harbourSize{ get; set; }
+    //     public string harbourSize{ get; set; }
 
-        public string harbourPosition{ get; set; }
+    //     // public string harbourPosition{ get; set; }
 
-        public string riverDistance{ get; set; }
+    //     public string riverDistance{ get; set; }
 
-        public string riverName{ get; set; }
+    //     public string riverName{ get; set; }
 
-        public string riverPosition{ get; set; }
+    //     // public string riverPosition{ get; set; }
 
-        public string nearDischarge{ get; set; }
+    //     public string nearDischarge{ get; set; }
 
-        public string dischargeDistance{ get; set; }
+    //     public string dischargeDistance{ get; set; }
 
-        public string dischargePosition{ get; set; }
+    //     // public string dischargePosition{ get; set; }
 
-        public string cleanedHowOften{ get; set; }
+    //     public string cleanedHowOften{ get; set; }
 
-        public string cleanedMonths{ get; set; }
+    //     // public string cleanedMonths{ get; set; }
 
-        public string cleanedOther{ get; set; }
+    //     public string cleanedOther{ get; set; }
 
-        public string cleanedYearRoundOrSeasonal{ get; set; }
+    //     public string cleanedYearRoundOrSeasonal{ get; set; }
 
-        public string cleaningMethod{ get; set; }
+    //     public string cleaningMethod{ get; set; }
         
-        public string responsibleForCleaning{ get; set; }
+    //     public string responsibleForCleaning{ get; set; }
 
-        public string additionalComments{ get; set; }
+    //     public string additionalComments{ get; set; }
 
-        public string amendment{ get; set; }
+    //     public string amendment{ get; set; }
 
-        public string dateCompleted{ get; set; }
+    //     public string dateCompleted{ get; set; }
 
-        public string enteredBy{ get; set; }
+    //     public string enteredBy{ get; set; }
 
-        public string enteredByPhone{ get; set; }
+    //     public string enteredByPhone{ get; set; }
 
-        public string enteredByEmail{ get; set; }
+    //     public string enteredByEmail{ get; set; }
 
-        public string CreatedBy { get; init; }
-        public List<SurveyDTO> Surveys { get; init; }
+           public string CreatedBy { get; init; }
+    //     public List<SurveyDTO> Surveys { get; init; }
     };
 }
